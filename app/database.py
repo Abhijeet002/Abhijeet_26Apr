5a61ea66b08045b55baabb4cc0771474cb8ad82a
Abhijeet <abhijeetsachan.088@gmail.com>
2025-04-23 20:28:21 +0530

    project initialisation

--- app/database.py
+++ app/database.py
@@ -4,3 +4,22 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
+
+load_dotenv()              #load environment variables}
+
+DATABASE_URL= os.getenv("DB_URL")      #read database url from env file
+
+engine= create_engine(DATABASE_URL)    #creating a SQLAlchemy  database engine 
+
+# Create session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
