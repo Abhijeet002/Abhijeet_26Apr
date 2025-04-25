ee672e9ac52d06fd25c5473e7ed654bf685b2dad
Abhijeet <abhijeetsachan.088@gmail.com>
2025-04-26 03:23:00 +0530

    update models.py <add Report table>

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
+# Creating tables for each csv to store data in postgre database
+
 from app.database import Base
-from sqlalchemy import Column, Integer, String, DateTime, Time
+from sqlalchemy import Column, Integer, String, DateTime, Time, Enum
+import enum
 
 
 # Creating tables
@@ -9,7 +12,8 @@ class BusinessHour(Base):
     store_id = Column(Integer, index= True)
     day =Column(Integer)
     start_time_local = Column(Time)
-    end = Column(Time)
+    end_time_local = Column(Time)
+
 
     
 
@@ -28,5 +32,27 @@ class StoreTimeZone(Base):
     store_id = Column(String, unique=True, index=True)
     timezone_str = Column(String)  
 
+class Report(Base):
+    __tablename__ = "reports"
+    id = Column(Integer, primary_key=True, index=True)
+    store_id = Column(String, index=True)
+    uptime_last_hour = Column(Integer)      # in minutes
+    uptime_last_day = Column(Integer)       # in minutes
+    uptime_last_week = Column(Integer)      # in minutes
+    downtime_last_hour = Column(Integer)    # in minutes
+    downtime_last_day = Column(Integer)     # in minutes
+    downtime_last_week = Column(Integer)    # in minutes
+    report_generated_at = Column(DateTime)  # When report was created
 
 
+class ReportStatusEnum(enum.Enum):
+    running = "Running"
+    complete = "Complete"
+
+class ReportTracker(Base):
+    __tablename__ = "report_tracker"
+
+    id = Column(Integer, primary_key=True, index=True)
+    report_id = Column(String, unique=True, index=True)
+    status = Column(Enum(ReportStatusEnum), default=ReportStatusEnum.running)
+    file_path = Column(String, nullable=True)
